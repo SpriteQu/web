58a351aa0a5790bf001f09d05a8cd49ef18e1058
spriteQi <sprite.q@foxmail.com>
2026-07-16 14:54:10 +0800

    Update index.js

--- idle-maple/src/router/index.js
+++ idle-maple/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Login from '../views/Login.vue'
 import Home from '../views/Home.vue'
 import { useWebSocketStore } from '../stores/websocketStore'
@@ -19,7 +19,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 })
 
